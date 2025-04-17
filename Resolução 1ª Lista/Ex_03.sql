37794be1b95fe52ca7fe03e6263b8cfc14b757e9
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2025-04-17 19:15:23 -0300

    Delete

--- Resolução 1ª Lista/Ex_03.sql
+++
@@ -1,8 +0,0 @@
-SET SERVEROUTPUT ON;
-DECLARE
-    total_alunos NUMBER;
-BEGIN
-    SELECT COUNT(*) INTO total_alunos FROM ALUNO;
-    DBMS_OUTPUT.PUT_LINE('Total de alunos cadastrados: ' || total_alunos);
-END;
-/
